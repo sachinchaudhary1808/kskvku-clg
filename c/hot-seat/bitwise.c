58a39b65d5c210ca215a76ef339e6f767645bc2f
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-07-24 11:43:08 +0530

    bleh

--- c/hot-seat/bitwise.c
+++ c/hot-seat/bitwise.c
@@ -4,4 +4,34 @@ CP(3): bitwise operator*/
 
 #include <stdio.h>
 
-void main() {}
+void main() {
+  // BITWISE OPERATORS = special operators used in bit level programming
+
+  // & = AND
+  // | = OR
+  // ^ = XOR
+  // << left shift
+  // >> right shift
+
+  int x = 2; // 6 = 00000110
+  int y = 4; // 12 = 00001100
+  int z = 0; // 24= 00000011
+
+  z = x & y;
+  printf("AND = %d\n", z);
+
+  z = x | y;
+  printf("OR = %d\n", z);
+
+  z = x ^ y;
+  printf("XOR = %d\n", z);
+
+  z = x << 2;
+  printf("left shift = %d\n", z);
+
+  z = x >> 1;
+  printf("right shift = %d\n", z);
+
+  z = ~x;
+  printf("invert = %d\n", z);
+}
